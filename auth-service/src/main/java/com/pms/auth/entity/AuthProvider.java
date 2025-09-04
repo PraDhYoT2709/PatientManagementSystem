59c0ed1ebe694e258871cf1a0b8c20de6901b14f
Pradhyot Prakhaar <pradhyot.pr2709@gmail.com>
2025-09-04 11:58:31 +0530

    Delete auth-service directory

--- auth-service/src/main/java/com/pms/auth/entity/AuthProvider.java
+++
@@ -1,6 +0,0 @@
-package com.pms.auth.entity;
-
-public enum AuthProvider {
-    LOCAL,
-    GOOGLE
-}
--- auth-service/src/main/java/com/pms/auth/entity/RoleName.java
+++
@@ -1,7 +0,0 @@
-package com.pms.auth.entity;
-
-public enum RoleName {
-    ADMIN,
-    DOCTOR,
-    PATIENT
-}
